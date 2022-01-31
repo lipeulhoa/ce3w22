ed2f75e738e478ef6b1e2880d3fca63ff2f934a9
Luiz Veiga <luizveiga@hotmail.com>
2022-01-30 22:56:15 -0500

    Initial upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//WHEN LOADING IS FINISHED, TRIGGER "START"
+$(window).on("load", start); 
+
+function start(){
+    $("body").css("background-color", "pink");
+    $("ul.nav a:first").click();
+}
+
+$(document).on("click", "ul.nav a", nav);
+
+function nav(e){
+    e.preventDefault();
+    //alert("Hello world");
+    var url = $(this).attr("href");
+    $("#content").load(url);
+    $("ul.nav a").removeClass("active");
+    $(this).addClass("active");
+}
+
